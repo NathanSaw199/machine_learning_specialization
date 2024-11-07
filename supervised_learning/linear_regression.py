752ef4d011e4f1c1087eb5fd6356a66249ff5140
Nathan <104150310@student.swin.edu.au>
2024-11-07 16:06:56 +1100

    add code structures

--- supervised_learning/linear_regression.py
+++ supervised_learning/linear_regression.py
@@ -5,21 +5,42 @@ plt.style.use(r"/Users/nathansaw/Desktop/untitled folder/machine_learning_specia
 
 # x_train = np.array([1.0, 1.7, 2.0, 2.5, 3.0, 3.2])
 # y_train = np.array([250, 300, 480,  430,   630, 730,])
-def compute_cost(x,y,w,b):
 
-     m = x.shape[0] 
-     cost_sum = 0 
-     for i in range(m): 
-          f_wb = w * x[i] + b   
-          cost = (f_wb - y[i]) ** 2  
-          cost_sum = cost_sum + cost  
-     total_cost = (1 / (2 * m)) * cost_sum  
-     return total_cost
 x_train = np.array([1.0,2.0])
 y_train = np.array([300,500])
-w = 150
+def cost_compute(x,y,w,b):
+    total_samples = x.shape[0]
+    the_sum_of_cost = 0
+    for i in range(total_samples):
+        f_x = (w*x[i])+b
+        print(f"f_x : {f_x}")
+        print(f"x[i] : {x[i]}")
+        c_function = (f_x - y[i])**2
+        print(f"y[i] : {y[i]}")
+        the_sum_of_cost+= c_function
+    total_cost = (1/(2*total_samples))*the_sum_of_cost
+    return total_cost
+
+w= 150
 b = 100
-# for For x[0] = 1.0, the model's prediction is:
+house_price = cost_compute(x_train,y_train,w,b)
+print(house_price)
+        
+# def compute_cost(x,y,w,b):
+
+#      m = x.shape[0] 
+#      cost_sum = 0 
+#      for i in range(m): 
+#           f_wb = w * x[i] + b   
+#           cost = (f_wb - y[i]) ** 2  
+#           cost_sum = cost_sum + cost  
+#      total_cost = (1 / (2 * m)) * cost_sum  
+#      return total_cost
+# x_train = np.array([1.0,2.0])
+# y_train = np.array([300,500])
+# w = 150
+# b = 100
+# # for For x[0] = 1.0, the model's prediction is:
 #b=w⋅x[0]+b=150⋅1.0+100=250
 #The squared error for this data point is:
 #cost=(250−300)**2=(−50)**2 =2500
@@ -32,8 +53,8 @@ b = 100
 #Final total cost:
 #total_cost= 1/(2⋅2)⋅12500=1/4*12500=3125.0
 
-cur_cost = compute_cost(x_train,y_train,w,b)
-print(f"curent cost: {cur_cost}")
+# cur_cost = compute_cost(x_train,y_train,w,b)
+# print(f"curent cost: {cur_cost}")
 # plt.close('all') 
 # fig, ax, dyn_items = plt_stationary(x_train, y_train)
 # updater = plt_update_onclick(fig, ax, x_train, y_train, dyn_items)
