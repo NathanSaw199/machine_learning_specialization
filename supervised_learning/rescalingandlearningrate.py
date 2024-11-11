082a3b12d448540768514f882b16621dfff97992
NathanSaw199 <104150310@student.swin.edu.au>
2024-11-12 01:24:00 +1100

    plotting

--- supervised_learning/rescalingandlearningrate.py
+++ supervised_learning/rescalingandlearningrate.py
@@ -13,4 +13,12 @@ for i in range(len(ax)):
     ax[i].scatter(X_train[:,i],y_train)
     ax[i].set_xlabel(X_features[i])
 ax[0].set_ylabel('price(1000s)')
-plt.show()
+# plt.show()
+#set alpha to 9.9e-7
+# _,_,hist = run_gradient_descent(X_train, y_train, 10, alpha = 9.9e-7)
+#set alpha to 9e-7
+
+# _,_,hist = run_gradient_descent(X_train, y_train, 10, alpha = 9e-7)
+#set alpha to 1e-7
+_,_,hist = run_gradient_descent(X_train, y_train, 10, alpha = 1e-7)
+plot_cost_i_w(X_train, y_train, hist)
