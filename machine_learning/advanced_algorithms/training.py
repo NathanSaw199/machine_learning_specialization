e1718d9ebd1850c7962f29900d3ac9bdab99ab09
NathanSaw199 <104150310@student.swin.edu.au>
2024-11-21 23:46:18 +1100

    lab_utils_softmax

--- machine_learning/advanced_algorithms/training.py
+++ machine_learning/advanced_algorithms/training.py
@@ -1,16 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.gridspec import GridSpec
 plt.style.use(r"C:\Users\Saw\Desktop\machine learning\machine_learning_specialization\machine_learning\advanced_algorithms\deeplearning.mplstyle")
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LeakyReLU
-from tensorflow.keras.activations import linear, relu, sigmoid
+from tensorflow.keras.layers import Dense
+from IPython.display import display, Markdown, Latex
+from sklearn.datasets import make_blobs
+%matplotlib widget
 from matplotlib.widgets import Slider
 from lab_utils_common import dlc
-from autils import plt_act_trio
-from lab_utils_relu import *
-import warnings
-warnings.simplefilter(action='ignore', category=UserWarning)
-plt_act_trio()
-_ = plt_relu_ex()
+from lab_utils_softmax import plt_softmax
+import logging
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+tf.autograph.set_verbosity(0)
